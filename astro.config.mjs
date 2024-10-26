677882d0869eceff6270b11d2506a02604c12a11
Raymond <r.boerema21@gmail.com>
2024-10-26 20:11:38 +0200

    finish site only add images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 import pagefind from "astro-pagefind";
+import react from "@astrojs/react";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), partytown(), pagefind()]
+  integrations: [icon(), partytown(), pagefind(), react()]
 });
